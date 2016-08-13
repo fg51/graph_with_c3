ecc2b428153181fdaa028581b25c91b45c14ac56
jiita <zknsftk1msrk@gmail.com>
2016-08-13 19:16:41 +0900

    fix

--- src/app.ts
+++ src/app.ts
@@ -1,22 +1,36 @@
 /// <reference path = "../typings/index.d.ts" />
 
 let chart = c3.generate({
+    //size: {
+    //    height: 800,
+    //    width: 600
+    //},
     bindto: "#chart",
     data: {
-        url: "/data.json",
+        url: "/data/data.json",
         mimeType: "json",
 
-        type: "line",
+        type: "line",   //NOTE: chart type
         keys: {
             x: "time",
-            value: ["velocity", "height"]
-        }
-    },
+            value: ["velocity", "height"]   //NOTE: "key name" 
+        },
+        axes: { //NOTE: "value name": "target axes"
+            velocity:"y",
+            height:"y2"
+        } },
     axis: {
         x: {
             tick: {
                 values: [0, 0.2, 0.4, 0.6, 0.8, 1.0]
             }
+        },
+        y: {
+            label: "Height [m]"
+        },
+        y2: {
+            show: true,
+            label: "Velocity [m/s]"
         }
     }
 });
