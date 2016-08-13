aa77d51bd547c7742ed46f4c0503037f18f32a48
jiita <zknsftk1msrk@gmail.com>
2016-08-13 22:53:52 +0900

    minor fixes
    fix d3 error @ typings.json
    add axes-y2 @ src/app.ts
    add line-size @ index.html

--- src/app.ts
+++ src/app.ts
@@ -3,13 +3,17 @@
 let chart = c3.generate({
     bindto: "#chart",
     data: {
-        url: "/data.json",
+        url: "/data/data.json",
         mimeType: "json",
 
         type: "line",
         keys: {
             x: "time",
             value: ["velocity", "height"]
+        },
+        axes: {
+            velocity: "y",
+            height: "y2"
         }
     },
     axis: {
@@ -17,8 +21,14 @@ let chart = c3.generate({
             tick: {
                 values: [0, 0.2, 0.4, 0.6, 0.8, 1.0]
             }
+        },
+        y: {
+            label: "Height [m]"
+        },
+        y2: {
+            show: true,
+            label: "Velocity [m/s]"
         }
     }
 });
-
 
